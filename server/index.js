71d8f026273244507ecc456f1b035b4c9b9d6448
robbyweeds <96435093+robbyweeds@users.noreply.github.com>
2025-06-20 18:34:33 -0400

    added new end point hello

--- server/index.js
+++ server/index.js
@@ -6,5 +6,9 @@ app.get('/', (req, res) => {
   res.send('Hello from server!');
 });
 
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from server!' });
+});
+
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
